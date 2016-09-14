152d28f2ec1846bfc73326e727b2b2fa07e0ba6f
Adekusiek <kawahara0114@gmail.com>
2016-09-14 21:26:13 +0900

    My first commitment

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,19 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(current_user)
+  end
+
+  def edit
+  end
+
+  def update
+    current_user.update(update_params)
+    redirect_to user_path(current_user)
+  end
+
+  private
+  def update_params
+    params.require(:user).permit(:family_name, :first_name, :avatar)
+  end
+
+end
