27e102be149c686256fbcac85680df47bd693e30
Adekusiek <kawahara0114@gmail.com>
2016-09-24 16:26:57 +0900

    Display result of scraping on the way

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,7 @@
 class TopController < ApplicationController
-  
+
   def index
+    @rposters = Poster.order(created_at: :DESC).limit(6)
+    @likedposters = Poster.order(created_at: :ASC).limit(6)
   end
 end
